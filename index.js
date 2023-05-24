41fdab8741e647d89e9186e25fea85e2a791c942
balavenkat123 <balavenkat17902@gmail.com>
2023-05-24 17:32:27 +0530

    counter

---
+++ index.js
@@ -0,0 +1,35 @@
+let count=0
+function increment(){
+    count=count+1
+    document.getElementById("count").innerText= count
+    console.log(count);
+    if (count==30){
+        document.getElementById("people").innerText="BALA"
+}  
+}  
+
+function save(){
+    
+    document.getElementById("saved").innerText+=" " + count+" - "
+
+
+}
+  
+//document.getElementById("count").innerText=10;
+
+// let count=0;
+// console.log(counnt);
+
+// var myage=20;
+// var dogage=5;
+
+// let mul=myage*dogage;
+// console.log(mul);
+
+// let bonuspoints=50
+ 
+// bonuspoints=bonuspoints+50
+// bonuspoints=bonuspoints-75
+// console.log(bonuspoints);
+// bonuspoints=bonuspoints+45
+// console.log(bonuspoints);
